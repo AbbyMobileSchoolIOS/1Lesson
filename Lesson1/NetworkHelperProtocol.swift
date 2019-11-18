e74250f60713beddf8f45669527a7b469752d0ee
Denis <d3nisk466@gmail.com>
2019-11-18 20:25:09 +0300

    add task 4,5

--- Lesson1/NetworkHelperProtocol.swift
+++ Lesson1/NetworkHelperProtocol.swift
@@ -7,29 +7,48 @@ public typealias OperationCompletion<A> = Result<A, Error>
 
 /// Протокол для работы с сервером на основе ресурсов
 public protocol NetworkHelperProtocol: class {
-	/// Метод загрузки данных на основе Resource(ресурс далее)
-	///
-	/// - Parameters:
-	///   - resource: ресурс
-	///   - completion: результат выполнения операции
-	/// - Returns: объект отмены операции загрузки данных
-	func load<A>(resource: Resource<A>, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation?
-	
-	/// Метод загрузки данных на основе Resource(ресурс далее) с возможностью повторения
-	///
-	/// - Parameters:
-	///   - resource: ресурс
-	///   - repeatTime: количество повторов
-	///   - delayTime: задержка между повторами
-	///   - completion: результат выполнения операции
-	/// - Returns: объект отмены операции загрузки данных
-	func load<A>(resource: Resource<A>, repeatTime: Int, delayTime: TimeInterval, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation?
+    /// Метод загрузки данных на основе Resource(ресурс далее)
+    ///
+    /// - Parameters:
+    ///   - resource: ресурс
+    ///   - completion: результат выполнения операции
+    /// - Returns: объект отмены операции загрузки данных
+    func load<A>(resource: Resource<A>, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation?
+    
+    /// Метод загрузки данных на основе Resource(ресурс далее) с возможностью повторения
+    ///
+    /// - Parameters:
+    ///   - resource: ресурс
+    ///   - repeatTime: количество повторов
+    ///   - delayTime: задержка между повторами
+    ///   - completion: результат выполнения операции
+    /// - Returns: объект отмены операции загрузки данных
+    func load<A>(resource: Resource<A>, repeatTime: Int, delayTime: TimeInterval, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation?
 }
+
 
 // MARK: - Реализация методов по умолчанию
 extension NetworkHelperProtocol {
-	// Реализация метода загрузки с повторениями(*)
-	public func load<A>(resource: Resource<A>, repeatTime: Int, delayTime: TimeInterval, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation? {
-		#error("Реализовать метод")
-	}
+    // Реализация метода загрузки с повторениями(*)
+    public func load<A>(resource: Resource<A>, repeatTime: Int, delayTime: TimeInterval, completion: @escaping (OperationCompletion<A>) -> ()) -> Cancellation? {
+        var repeatLeft = repeatTime
+        let cancelContainer = RequestCancelContainer()
+        
+        Timer.scheduledTimer(withTimeInterval: delayTime, repeats: true){ timer in
+            cancelContainer.requestCancel = self.load(resource: resource, completion: { result in
+                switch result{
+                case .success:
+                    completion(result)
+                    timer.invalidate()
+                case .failure:
+                    repeatLeft -= 1
+                    if(repeatLeft == 0){
+                        completion(result)
+                        timer.invalidate()
+                    }
+                }
+            })
+        }
+        return cancelContainer
+    }
 }
